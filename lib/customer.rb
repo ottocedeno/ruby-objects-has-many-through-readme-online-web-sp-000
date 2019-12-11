da4a5e9fae3e369a3ccc57cc31ba470c437db46f
Otto Cedeno <otto@ottostacos.com>
2019-12-11 01:36:26 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -19,6 +19,12 @@ class Customer
     Meal.new(waiter, self, total, tip)
   end
 
+  def meals
+  Meal.all.select do |meal|
+    meal.customer == self
+  end
+end
+
 end
 
 otto = Customer.new("Otto", 34)
